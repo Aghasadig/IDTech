14c758b6162a2149af318b96e6a75bab2122ffcd
Aghasadig <aghasadig.gurbanov@gmail.com>
2025-01-13 01:39:07 +0400

    JS task 1

---
+++ JS/main.js
@@ -0,0 +1,59 @@
+// 1
+
+
+
+
+
+//  2
+
+// function removeElements(array) {
+    
+//     const result = array.slice(3);
+//     console.log(result); 
+//   }
+  
+ 
+//   const myArray = [1, 2, 3, 4, 5, 6];
+//   removeElements(myArray); 
+  
+  
+
+
+
+//  3
+
+// function removeElement(array=[], a) {
+//     const filteredArray = array.filter(element => element !== a);
+//     return filteredArray; 
+//   }
+  
+ 
+//   console.log(removeElement([1, 2, 3], 2)); 
+
+
+
+// // 4
+
+// function countNegativeValues(array) {
+
+//     const negativeValues = array.filter(element => element < 0);
+//     return negativeValues;
+//   }
+  
+
+//   console.log(countNegativeValues([1, -2, 2, -4])); 
+
+
+// // 5
+
+// function calculateAverage(array) {
+   
+//     const sum = array.reduce((acc, curr) => acc + curr, 0);
+    
+
+//     const average = sum / array.length;
+    
+//     return average; 
+//   }
+  
+//   console.log(calculateAverage([10, 100, 45])); 
