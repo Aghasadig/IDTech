43fea514efec5267dd12c4ce6cc691076e6629dc
Aghasadig <aghasadig.gurbanov@gmail.com>
2025-01-29 22:09:19 +0400

    LAB 5 DONE

--- JS/Lab5/main.js
+++ JS/Lab5/main.js
@@ -2,6 +2,32 @@ const headers = document.querySelectorAll('.accordion-item-header');
 
   headers.forEach((item)=>{
     item.addEventListener("click", ()=>{
-        item.classList.toggle("                                     ")
+        item.classList.toggle("active")
     })
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
