a0e24ce9d95c76c674841ff04498eec4ab53768c
Aghasadig <aghasadig.gurbanov@gmail.com>
2025-02-16 21:39:24 +0400

    Age Calculator

--- JS_Infinity_Lab/ageCalculatorApp/main.js
+++ JS_Infinity_Lab/ageCalculatorApp/main.js
@@ -5,42 +5,37 @@ const months = document.querySelector("#months");
 const days = document.querySelector("#days");
 
 form.addEventListener("submit", (e) => {
-    const formData = new FormData(e.target);
-    e.preventDefault();
+  const formData = new FormData(e.target);
+  e.preventDefault();
 
-    const day = formData.get("day");
-    const month = formData.get("month");
-    const year = formData.get("year");
+  const day = formData.get("day");
+  const month = formData.get("month");
+  const year = formData.get("year");
 
-    
-    const birthday = new Date(year,month,day)
-    const dateObj = calculateDateDifference(birthday);
-     
-    years.innerHTML = dateObj.years;
-    months.innerHTML = dateObj.months;
-    days.innerHTML = dateObj.days;
-    
-    
-})
+  const birthday = new Date(year, month, day);
+  const dateObj = calculateDateDifference(birthday);
+
+  years.innerHTML = dateObj.years;
+  months.innerHTML = dateObj.months;
+  days.innerHTML = dateObj.days;
+});
 
 function calculateDateDifference(date) {
-    const today = new Date();
-    let years = today.getFullYear() - date.getFullYear();
-    let months = today.getMonth() - date.getMonth();
-    let days = today.getDate() - date.getDate();
+  const today = new Date();
+  let years = today.getFullYear() - date.getFullYear();
+  let months = today.getMonth() - date.getMonth();
+  let days = today.getDate() - date.getDate();
 
-    if (days < 0) {
-        months -= 1;
-        const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
-        days += lastMonth.getDate();
-    }
+  if (days < 0) {
+    months -= 1;
+    const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    days += lastMonth.getDate();
+  }
 
-    if (months < 0) {
-        years -= 1;
-        months += 12;
-    }
+  if (months < 0) {
+    years -= 1;
+    months += 12;
+  }
 
-    return { years, months, days };
+  return { years, months, days };
 }
-
-
